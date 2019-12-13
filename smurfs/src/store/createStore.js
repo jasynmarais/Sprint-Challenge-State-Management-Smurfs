c57fd19640be0cfcfe5eec3002edb84d35575568
Jasyn Marais <jasynmarais@gmail.com>
2019-12-13 11:40:34 +0200

    Configure Redux Devtools Extension

--- smurfs/src/store/createStore.js
+++ smurfs/src/store/createStore.js
@@ -1,6 +1,12 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
 import { reducer } from '../reducers';
 
-export const store = createStore(reducer, applyMiddleware(thunk));
+export const store = createStore(
+    reducer, 
+    compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+);
